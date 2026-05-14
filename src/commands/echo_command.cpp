b51e5967e983e9c57e8130ebde9adc1087914707
ivan <medvedevivan550@gmail.com>
2026-05-14 17:28:27 +0300

    codecrafters submit [skip ci]

--- src/commands/echo_command.cpp
+++ src/commands/echo_command.cpp
@@ -1,19 +1,40 @@
 #include "echo_command.hpp"
 
 #include <print>
-
 #include <vector>
 #include <string>
 #include <ranges>
+#include <fstream>
 
 
 void EchoCommand::Execute(const std::vector<std::string> &args) {
-    for (size_t i = 0; i < args.size(); ++i) {
-        std::print("{}", args[i]);
+    const RedirectCommand cmd = ParseEchoCommand(args);
+    const auto result = cmd.args | std::views::join_with(' ') | std::ranges::to<std::string>();
+    switch (cmd.mode) {
+        case RedirectMode::None:
+            std::println("{}", result);
 
-        if (i < args.size() - 1) {
-            std::print(" ");
+        case RedirectMode::Overwrite:
+            std::ofstream file(cmd.outputFile);
+            file << result;
+    }
+}
+
+
+RedirectCommand EchoCommand::ParseEchoCommand(const std::vector<std::string> &args) const {
+     RedirectCommand cmd;
+    for (size_t i = 0; i < args.size(); ++i) {
+        if (args[i] == ">" || args[i] == "1>") {
+            if (i + 1 < args.size()) {
+                cmd.mode = RedirectMode::Overwrite;
+                cmd.outputFile = args[++i];
+            }
+        }
+
+        else {
+            cmd.args.push_back(args[i]);
         }
     }
-    std::println();
+
+    return cmd;
 }
--- src/commands/echo_command.hpp
+++ src/commands/echo_command.hpp
@@ -2,7 +2,25 @@
 
 #include "commands/base_command.hpp"
 
+
+namespace {
+    enum class RedirectMode {
+        None,
+        Overwrite,
+    };
+
+    struct RedirectCommand {
+        std::vector<std::string> args;
+        std::string outputFile;
+        RedirectMode mode = RedirectMode::None;
+    };
+}
+
+
 class EchoCommand : public ICommand {
 public:
     void Execute(const std::vector<std::string> &args) override;
+
+private:
+    RedirectCommand ParseEchoCommand(const std::vector<std::string> &args) const;
 };
